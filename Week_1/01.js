dde82045b4fd9c070f34bb06ee9f2cc37e56fe95
Jo <johj703@nate.com>
2024-08-08 14:16:11 +0900

    var를 사용하면 안되는 이유다.

--- Week_1/01.js
+++ Week_1/01.js
@@ -1,8 +1,5 @@
-let blockScopeVariable = "Available only in this block";
-if (true) {
-    var blockedScope = "Visible inside this block";
-    console.log(blockedScope);
+for (var i = 0; i < 3; i++) {
+    console.log(i)
 }
-console.log(blockedScope)
-console.log(blockScopeVariable)
 
+console.log(i)
