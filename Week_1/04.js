bcb4ca3923a911eb6630559d9f8e02ddab86c364
Jo <johj703@nate.com>
2024-08-08 15:59:17 +0900

    entries를 통해 객체의 속성을 key-value 한 쌍씩 배열로 나타내준다!

--- Week_1/04.js
+++ Week_1/04.js
@@ -11,3 +11,7 @@ console.log(keys);
 // (2) Object.values()
 const values = Object.values(user);
 console.log(values);
+
+// (3) entries - 배열 안에 배열을 만들어서 속성별로 Key - Value 값을 나타내준다.
+const entries = Object.entries(user);
+console.log(entries);
