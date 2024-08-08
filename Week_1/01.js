c8823259de1bd7834c9a022b463dbf1b358ebdf5
Jo <johj703@nate.com>
2024-08-08 14:14:00 +0900

    var로 변수를 선언하고 할당을 하면 함수 바깥에서도 해당 변수를 사용할 수 있다.

--- Week_1/01.js
+++ Week_1/01.js
@@ -1,7 +1,8 @@
 let blockScopeVariable = "Available only in this block";
 if (true) {
-    let blockedScope = "Visible inside this block";
+    var blockedScope = "Visible inside this block";
     console.log(blockedScope);
 }
 console.log(blockedScope)
 console.log(blockScopeVariable)
+
