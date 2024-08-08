689971f0eacd83afb2d8e9e77e9971bcd02cfcaf
Jo <johj703@nate.com>
2024-08-08 14:32:14 +0900

    객체에 접근할 때에는 "객체.접근하고자하는 요소" 로 입력하면 된다!

---
+++ Week_1/02.js
@@ -0,0 +1,13 @@
+// 객체 : key - value
+// JSON(JavaScript Object Notation)
+
+const user = {
+    name: "르탄이",
+    age: 30,
+    isAdmen: true,
+    printHello: () => console.log("hello"),
+};
+
+// 객체 접근
+console.log(user.name);
+console.log(user.age);
