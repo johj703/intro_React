6b6c153a437e09d4ef254887650e952a83a2f848
Jo <johj703@nate.com>
2024-08-08 20:59:13 +0900

    문제1, 2번 풀이

--- Week_1/05.js
+++ Week_1/05.js
@@ -43,19 +43,19 @@ const item = {
 
 // const name = item.name;
 // const price = item.price;
-const { name, price } = item;
+// const { name, price } = item;
 
-console.log("name => ", name);
-console.log("price => ", price);
+// console.log("name => ", name);
+// console.log("price => ", price);
 
-function greet({ age, name }) {
-    console.log(`안녕하세요. 제 이름은 ${name}입니다. 나이는 ${age}에요.`);
-}
+// function greet({ age, name }) {
+//     console.log(`안녕하세요. 제 이름은 ${name}입니다. 나이는 ${age}에요.`);
+// }
 
-const person = {
-    name: "르순이",
-    age: 28,
-};
+// const person = {
+//     name: "르순이",
+//     age: 28,
+// };
 // greet(person);
 
 // Spread Operators
@@ -65,4 +65,32 @@ const [, , thirdColor] = colors;
 
 // console.log(firstColor);
 // console.log(secondColor);
-console.log(thirdColor);
+// console.log(thirdColor);
+
+// 문제1
+/* 다음 자바스크립트 객체에서 name과 age를 디스트럭처링을 사용하여 추출하고 
+출력하는 코드를 작성하세요. */
+
+const person = {
+    name: "르탄이",
+    age: 25,
+    job: "개발자"
+};
+
+const { name, age } = person;
+// console.log(`안녕하세요 ${name}님, ${age}살 입니다.`)
+
+// 문제2
+/* 다음 자바스크립트 객체에서 title과 year를 디스트럭처링을 사용하여 추출하고
+ 출력하는 코드를 작성하세요. */
+const movie = {
+    title: "Inception",
+    director: "Christopher Nolan",
+    release: {
+        year: 2010,
+        month: "July"
+    }
+};
+
+const { title, release: { year }, } = movie;
+console.log(`영화의 제목은 ${title} / (${year}년도) 입니다.`)
