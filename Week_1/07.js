4aa57c626efddc07d8183885c759f0fee2c458e4
Jo <johj703@nate.com>
2024-08-08 23:26:21 +0900

    async - await 예시

--- Week_1/07.js
+++ Week_1/07.js
@@ -6,10 +6,35 @@ const myPromise = new Promise(function (resolve, reject) {
     }
 });
 
-myPromise
-    .then(function (value) {
-        console.log(value)
-    })
-    .catch(function (value) {
-        console.log(value)
-    })
+// myPromise
+//     .then(function (value) {
+//         console.log(value)
+//     })
+//     .catch(function (value) {
+//         console.log(value)
+//     })
+
+// async - await
+async function fetchData() {
+    return 'Data loaded';
+}
+
+// 아래 코드와 같아요.
+// async function fetchData() {
+// 	return Promise.resolve('Data loaded');
+// }
+
+// fetchData().then(console.log); // 'Data loaded'
+
+// await 사용의 이점
+async function fetchData() {
+    try {
+        const data = await fetch('https://api.example.com/data');
+        const json = await data.json();
+        console.log(json);
+    } catch (error) {
+        console.error("Data loading failed", error);
+    }
+}
+
+fetchData();
